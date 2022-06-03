87931719b85030581e48a01ef0b557a33ca8abd7
Débora Labes Zavadniak <dlzavadniak@furb.br>
2022-06-03 20:45:12 -0300

    POO

--- debora/Tarefa/src/classes/Checklistitem.java
+++ debora/Tarefa/src/classes/Checklistitem.java
@@ -1,2 +1,68 @@
-package classes;public class Checklistitem {
+package classes;
+
+import java.util.UUID;
+
+public class Checklistitem {
+
+    // atributos
+    private String uuid;
+
+    private String nome;
+
+    private String descricao;
+
+    private boolean completa;
+
+    private int ordem;
+
+
+    // metodos
+    public Checklistitem() {
+        this.setUuid(UUID.randomUUID().toString());
+    }
+
+    public void completar() {
+
+    }
+
+    // getter e setter
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isCompleta() {
+        return completa;
+    }
+
+    public void setCompleta(boolean completa) {
+        this.completa = completa;
+    }
+
+    public int getOrdem() {
+        return ordem;
+    }
+
+    public void setOrdem(int ordem) {
+        this.ordem = ordem;
+    }
 }
